82ab4e55fd777297150449c641c7eeb09b73921e
alanmrocha <alanmrocha@pop-os.localdomain>
2023-05-09 19:15:11 -0300

    refactor: app container added to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
+import { AppContainer } from "./styles";
 
 function App() {
-  return null;
+  return <AppContainer>Columns will go here</AppContainer>;
 }
 
 export default App;
