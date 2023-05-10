51eef19f4f71f510c2f542abf1f1553c1902e043
alanmrocha <alanmrocha@pop-os.localdomain>
2023-05-10 01:03:34 -0300

    refactor: few changes to improve component

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
+import { AddNewItem } from "./AddNewItem";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
   text: string;
-  children: string;
+  children: any;
 };
 
-export const Column: FC<ColumnProps> = ({ text, children }) => {
+export const Column: FC<ColumnProps> = ({ children, text }) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+      <AddNewItem
+        onAdd={console.log}
+        toggleButtonText={"+ Add another task"}
+        dark
+      />
     </ColumnContainer>
   );
 };
