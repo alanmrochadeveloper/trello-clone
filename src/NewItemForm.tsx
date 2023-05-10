af80201aacd806044fb77fa88a5869e4a904373d
alanmrocha <alanmrocha@pop-os.localdomain>
2023-05-10 01:03:50 -0300

    chore: added new item form

---
+++ src/NewItemForm.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles";
+import { useFocusInput } from "./utils/useFocus";
+
+type NewItemFormProps = {
+  onAdd(text: string): void;
+};
+
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState("");
+  const inputRef = useFocusInput();
+
+  return (
+    <NewItemFormContainer>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemFormContainer>
+  );
+};
