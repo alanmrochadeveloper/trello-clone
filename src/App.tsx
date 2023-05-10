bb70929dcde19e94091bc7bd7cc49c63b861cb93
alanmrocha <alanmrocha@pop-os.localdomain>
2023-05-10 01:02:32 -0300

    refactor: design the column and items in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from "react";
+import { AddNewItem } from "./AddNewItem";
 import { AppContainer } from "./styles";
+import { Column } from "./Column";
+import { Card } from "./Card";
 
 type AppType = {
   children?: React.Component;
 };
 
 const App: FC<AppType> = ({ children }) => {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 };
 
 export default App;
