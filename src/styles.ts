bc9fa3eba91f7d50de1d0df3c6f979dfaab57839
alanmrocha <alanmrocha@pop-os.localdomain>
2023-05-10 01:04:26 -0300

    style: new styleds components were add

--- src/styles.ts
+++ src/styles.ts
@@ -4,11 +4,47 @@ type AddItemButtonProps = {
   dark?: boolean;
 };
 
+export const NewItemFormContainer = styled.div`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export const NewItemButton = styled.button`
+  background-color: #5cac44;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+`;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e42 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
+
 export const AddItemButton = styled.button<AddItemButtonProps>`
-  background-color: #ffff3d;
+  background-color: #ffffd;
   border-radius: 3px;
   border: none;
   color: ${(props) => (props.dark ? "#000" : "#fff")};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #ffff52;
+  }
 `;
 
 export const AppContainer = styled.div`
